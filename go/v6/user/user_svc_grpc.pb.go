81f1ae421deeeb92e70cb1e396689136b568fb79
zzzhr <zzzhr@hotmail.com>
2023-08-28 13:50:09 +0800

    Auto commit

--- go/v6/user/user_svc_grpc.pb.go
+++ go/v6/user/user_svc_grpc.pb.go
@@ -19,11 +19,12 @@ import (
 const _ = grpc.SupportPackageIsVersion7
 
 const (
-	PubUser_Get_FullMethodName    = "/v6.services.pub.PubUser/Get"
-	PubUser_Login_FullMethodName  = "/v6.services.pub.PubUser/Login"
-	PubUser_Logoff_FullMethodName = "/v6.services.pub.PubUser/Logoff"
-	PubUser_Create_FullMethodName = "/v6.services.pub.PubUser/Create"
-	PubUser_Delete_FullMethodName = "/v6.services.pub.PubUser/Delete"
+	PubUser_Get_FullMethodName     = "/v6.services.pub.PubUser/Get"
+	PubUser_Login_FullMethodName   = "/v6.services.pub.PubUser/Login"
+	PubUser_Refresh_FullMethodName = "/v6.services.pub.PubUser/Refresh"
+	PubUser_Logoff_FullMethodName  = "/v6.services.pub.PubUser/Logoff"
+	PubUser_Create_FullMethodName  = "/v6.services.pub.PubUser/Create"
+	PubUser_Delete_FullMethodName  = "/v6.services.pub.PubUser/Delete"
 )
 
 // PubUserClient is the client API for PubUser service.
@@ -31,7 +32,8 @@ const (
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type PubUserClient interface {
 	Get(ctx context.Context, in *User, opts ...grpc.CallOption) (*User, error)
-	Login(ctx context.Context, in *User, opts ...grpc.CallOption) (*User, error)
+	Login(ctx context.Context, in *LoginRequest, opts ...grpc.CallOption) (*LoginResponse, error)
+	Refresh(ctx context.Context, in *LoginResponse, opts ...grpc.CallOption) (*LoginResponse, error)
 	Logoff(ctx context.Context, in *User, opts ...grpc.CallOption) (*User, error)
 	Create(ctx context.Context, in *User, opts ...grpc.CallOption) (*User, error)
 	Delete(ctx context.Context, in *User, opts ...grpc.CallOption) (*User, error)
@@ -54,9 +56,18 @@ func (c *pubUserClient) Get(ctx context.Context, in *User, opts ...grpc.CallOpti
 	return out, nil
 }
 
-func (c *pubUserClient) Login(ctx context.Context, in *User, opts ...grpc.CallOption) (*User, error) {
-	out := new(User)
+func (c *pubUserClient) Login(ctx context.Context, in *LoginRequest, opts ...grpc.CallOption) (*LoginResponse, error) {
+	out := new(LoginResponse)
 	err := c.cc.Invoke(ctx, PubUser_Login_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *pubUserClient) Refresh(ctx context.Context, in *LoginResponse, opts ...grpc.CallOption) (*LoginResponse, error) {
+	out := new(LoginResponse)
+	err := c.cc.Invoke(ctx, PubUser_Refresh_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -95,7 +106,8 @@ func (c *pubUserClient) Delete(ctx context.Context, in *User, opts ...grpc.CallO
 // for forward compatibility
 type PubUserServer interface {
 	Get(context.Context, *User) (*User, error)
-	Login(context.Context, *User) (*User, error)
+	Login(context.Context, *LoginRequest) (*LoginResponse, error)
+	Refresh(context.Context, *LoginResponse) (*LoginResponse, error)
 	Logoff(context.Context, *User) (*User, error)
 	Create(context.Context, *User) (*User, error)
 	Delete(context.Context, *User) (*User, error)
@@ -109,8 +121,11 @@ type UnimplementedPubUserServer struct {
 func (UnimplementedPubUserServer) Get(context.Context, *User) (*User, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Get not implemented")
 }
-func (UnimplementedPubUserServer) Login(context.Context, *User) (*User, error) {
+func (UnimplementedPubUserServer) Login(context.Context, *LoginRequest) (*LoginResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Login not implemented")
+}
+func (UnimplementedPubUserServer) Refresh(context.Context, *LoginResponse) (*LoginResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Refresh not implemented")
 }
 func (UnimplementedPubUserServer) Logoff(context.Context, *User) (*User, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Logoff not implemented")
@@ -153,7 +168,7 @@ func _PubUser_Get_Handler(srv interface{}, ctx context.Context, dec func(interfa
 }
 
 func _PubUser_Login_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(User)
+	in := new(LoginRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -165,7 +180,25 @@ func _PubUser_Login_Handler(srv interface{}, ctx context.Context, dec func(inter
 		FullMethod: PubUser_Login_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(PubUserServer).Login(ctx, req.(*User))
+		return srv.(PubUserServer).Login(ctx, req.(*LoginRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _PubUser_Refresh_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(LoginResponse)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(PubUserServer).Refresh(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: PubUser_Refresh_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(PubUserServer).Refresh(ctx, req.(*LoginResponse))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -238,6 +271,10 @@ var PubUser_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Login",
 			Handler:    _PubUser_Login_Handler,
+		},
+		{
+			MethodName: "Refresh",
+			Handler:    _PubUser_Refresh_Handler,
 		},
 		{
 			MethodName: "Logoff",
