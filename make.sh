9371f2e5c42de1f4d23d32d9aaade5d2228e1453
zzzhr <zzzhr@hotmail.com>
2024-02-27 00:01:55 +0800

    Auto commit

--- make.sh
+++ make.sh
@@ -8,7 +8,7 @@ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 GOPATH=$(go env GOPATH)
 export PATH=$PATH:$GOPATH/bin
 
-DIRS=("common" "slice" "user" "userfile" "offline" "rpcs" "wrtc" "share")
+DIRS=("common" "slice" "user" "userfile" "offline" "rpcs" "wrtc" "share" "webdav")
 
 
 rm -rf ./go_temp
